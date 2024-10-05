c3896068ea61ba331b02126432fe57714bb4ef3c
Swaliha-CA <swalihaswali100@gmail.com>
2024-10-05 11:19:50 +0530

    Add files via upload

---
+++ Tutorial/exception.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class exception
+{
+	public static void main (String[]args)
+	{
+		try
+			{
+				int[] num={1,2,3};
+			}
+		catch (Exception e)
+			{
+				System.out.println("error occured in program");
+			}
+		finally
+			{
+				System.out.println("Try and catch excecuted successfully");
+			}
+	}
+}
+	
+	
+
