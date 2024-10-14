a4640edb4994ab2e323e26fcb0f820e4ee76ded7
Swaliha-CA <swalihaswali100@gmail.com>
2024-10-14 09:24:33 +0530

    Add files via upload

---
+++ OOPS LAB/abstract.java
@@ -0,0 +1,39 @@
+abstract class shape{
+	public abstract void numberOfsides();
+	}
+		class Rectangle extends shape
+		{
+			public void numberOfsides()
+			{
+				System.out.println("Number of sides of rectangle=4");
+			}
+		}
+		class Triangle extends shape
+		{
+			public void numberOfsides()
+			{
+				System.out.println("Number of sides of triangle=3");
+			}
+		}
+		class Hexagon extends shape
+		{
+			public void numberOfsides()
+			{
+				System.out.println("Number of sides of hexagon=6");
+			}
+		}
+	
+	
+	class test
+	{
+		public static void main (String args[])
+		{
+			Rectangle R=new Rectangle();
+			Triangle T=new Triangle();
+			Hexagon H=new Hexagon();
+			R.numberOfsides();
+			T.numberOfsides();
+			H.numberOfsides();
+	        }
+	   }
+				
